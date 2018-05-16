fbd9c8cab7421d771ee68e43e0365666047615be
JohnZBurnett <tuninator@gmail.com>
2018-05-16 14:10:35 -0400

    write register methods

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+require 'pry'
+class CashRegister
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @item_list = []
+    @last_transaction = nil
+  end
 
+ attr_accessor :discount, :total
+
+  def add_item(title, price, quantity = 0)
+    if quantity != 0
+      @total += (price * quantity)
+      @last_transaction = price * quantity
+      quantity.times do
+        @item_list << title
+      end
+    else
+      @total += price
+      @last_transaction = price
+      @item_list << title
+    end
+  end
+
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      @total = @total - (@total * (@discount.to_f / 100))
+      return "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+
+  def items
+    return @item_list
+  end
+
+  def void_last_transaction
+    @total -= @last_transaction
+  end
+end
+
+register = CashRegister.new(20)
+register.total = 1000
+register.apply_discount
